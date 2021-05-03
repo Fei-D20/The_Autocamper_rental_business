67d486c84f32dc35b53549a69e8f8715ea5570b3
Evens <feix0033@easv365.dk>
2021-05-03 12:20:37 +0200

    make the jdbc connection

--- RantalSystem/src/com/Database/JDBC/ConnectionUtil.java
+++ RantalSystem/src/com/Database/JDBC/ConnectionUtil.java
@@ -1,5 +1,12 @@
 package com.Database.JDBC;
 
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.Properties;
+
 /**
  * @ author Fei Gu
  * @ create 2021-05-01-07.46
@@ -8,4 +15,84 @@ package com.Database.JDBC;
  * @ Version
  */
 public class ConnectionUtil {
+    @Test
+    public void testGetconnection() throws Exception {
+        Connection connection = ConnectionUtil.getConnection();
+        ConnectionUtil.closeConnection(connection);
+
+    }
+
+
+    public static Connection getConnection() throws Exception {
+        InputStream is = ClassLoader.getSystemClassLoader().getResourceAsStream("jdbc.properties");
+        Properties pros = new Properties();
+        pros.load(is);
+
+        String user = pros.getProperty("user");
+        String password = pros.getProperty("password");
+        String url = pros.getProperty("url");
+        String driverClass = pros.getProperty("driverClass");
+
+        Class.forName(driverClass);
+
+        Connection connection = DriverManager.getConnection(url, user, password);
+        System.out.println("The connection is successful : \n" + connection);
+        return connection;
+
+    }
+
+    public static void closeConnection(Connection connection){
+        try{
+            if(connection != null) {
+                connection.close();
+                System.out.println("the connection has been closed ! ");
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+
+    public static void closeConAndPS(Connection connection, Statement preparedStatement){
+        try {
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        try{
+            if(connection != null) {
+                connection.close();
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+
+
+    public static void closeConPSAndRS(Connection connection, Statement preparedStatement, ResultSet resultSet){
+        try {
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        try{
+            if(connection != null) {
+                connection.close();
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        try{
+            if(resultSet != null) {
+                resultSet.close();
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
 }
