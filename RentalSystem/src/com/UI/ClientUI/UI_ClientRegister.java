7ce724a1e864d398c47c3d6416c32ecbfa16afb1
Evens <feix0033@easv365.dk>
2021-05-05 09:06:22 +0200

    UI so far not so far away

--- RentalSystem/src/com/UI/ClientUI/UI_ClientRegister.java
+++ RentalSystem/src/com/UI/ClientUI/UI_ClientRegister.java
@@ -8,6 +8,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
 import javafx.scene.layout.*;
 import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 
@@ -22,53 +23,84 @@ public class UI_ClientRegister {
 
     public static Stage clientRegisterPage(Stage stage){
 
-        Label new_client_register = new Label("New Client Register");
-        new_client_register.setStyle("-fx-font-size: 20");
+        Label l_registerPageTitle = new Label("New Client Register");
+        l_registerPageTitle.setStyle("-fx-font-size: 20");
 
-        Label name = new Label("Your name : ");
-        name.setFont(Font.font(16));
+        Label l_name = new Label("Your name : ");
+        l_name.setFont(Font.font(16));
 
-        TextField userName = new TextField();
-        userName.setPromptText("Please input your full name");
-        userName.setTooltip(new Tooltip("Please input your full name"));
-        userName.textProperty().addListener(new Controller_ClientRegister().clientRegisterText());
-        userName.setPrefSize(300.0,10.0);
+        TextField tf_name = new TextField();
+        tf_name.setPromptText("Please input your full name");
+        tf_name.setTooltip(new Tooltip("Please input your full name"));
+        tf_name.textProperty().addListener(new Controller_ClientRegister().clientRegisterText());
+        tf_name.setPrefSize(300.0,10.0);
 
-        Label address = new Label("Your address : ");
-        address.setFont(Font.font(16));
+        Label l_address = new Label("Your address : ");
+        l_address.setFont(Font.font(16));
 
-        TextField userAddress = new TextField();
-        userAddress.setPromptText("Street,number,zipcode,city,country..");
-        userAddress.setTooltip(new Tooltip("Street,number,zipcode,city,country.."));
-        userAddress.setPrefSize(300.0,10.0);
+        TextField tf_address = new TextField();
+        tf_address.setPromptText("Street,number,zipcode,city,country..");
+        tf_address.setTooltip(new Tooltip("Street,number,zipcode,city,country.."));
+        tf_address.setPrefSize(300.0,10.0);
 
-        Label email = new Label("Your email : ");
-        email.setFont(Font.font(16));
+        Label l_email = new Label("Your email : ");
+        l_email.setFont(Font.font(16));
 
-        TextField userEmail = new TextField();
-        userEmail.setPromptText("email@mail.com");
-        userEmail.setTooltip(new Tooltip("email@mail.com"));
+        TextField tf_email = new TextField();
+        tf_email.setPromptText("email@mail.com");
+        tf_email.setTooltip(new Tooltip("email@mail.com"));
 
-        Label phoneNo = new Label("Your phone number : ");
-        phoneNo.setFont(Font.font(16));
+        Label l_phoneNo = new Label("Your phone number : ");
+        l_phoneNo.setFont(Font.font(16));
 
-        TextField userPhoneNo = new TextField();
+        TextField tf_phoneNo = new TextField();
 
-        userPhoneNo.setPromptText("xxxx-xxxxxxxx");
-        userPhoneNo.setTooltip(new Tooltip("xxxx-xxxxxxxx"));
+        tf_phoneNo.setPromptText("xxxx-xxxxxxxx");
+        tf_phoneNo.setTooltip(new Tooltip("xxxx-xxxxxxxx"));
 
-        Label label = new Label("I am realy tired to make more.... sorry ~~");
-        label.setFont(Font.font(18));
+        Label l_driveLicenseNo = new Label("Drive License no. : ");
+        l_driveLicenseNo.setFont(Font.font(16));
+
+        TextField tf_driveLicenseNo = new TextField();
+
+        Label l_driveFullDrivingLicenseStatue = new Label("Full driving license : ");
+        l_driveFullDrivingLicenseStatue.setFont(Font.font(16));
+        Text t_driveFullDrivingLicenseStatue = new Text();
+        String s_fullDrivingLicenseExp = "Do you have been held the driving\n license in your country of domicile\n for at least 12 month?";
+        t_driveFullDrivingLicenseStatue.setText(s_fullDrivingLicenseExp);
+
+        TextField tf_driveFullDrivingLicenseStatue = new TextField();
 
 
         GridPane gridPane = new GridPane();
         gridPane.setStyle("-fx-background-color: lightgray");
 
-        gridPane.add(new_client_register,0,0);
-        gridPane.add(name,0,1);
-        gridPane.add(userName,1,1);
-        gridPane.add(address,0,2);
-        gridPane.add(userAddress,1,2);
+        gridPane.add(l_registerPageTitle,0,0);
+
+        gridPane.add(l_name,0,1);
+        gridPane.add(tf_name,1,1);
+
+        gridPane.add(l_address,0,2);
+        gridPane.add(tf_address,1,2);
+
+        gridPane.add(l_email,0,3);
+        gridPane.add(tf_email,1,3);
+
+        gridPane.add(l_phoneNo,0,4);
+        gridPane.add(tf_phoneNo,1,4);
+
+        gridPane.add(l_driveLicenseNo,0,5);
+        gridPane.add(tf_driveLicenseNo,1,5);
+
+        gridPane.add(l_driveFullDrivingLicenseStatue,0,6);
+        gridPane.add(tf_driveFullDrivingLicenseStatue,1,6);
+
+        gridPane.add(t_driveFullDrivingLicenseStatue,1,7);
+
+
+
+
+
 
         gridPane.setHgap(10);
         gridPane.setVgap(10);
